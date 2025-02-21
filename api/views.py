d86e1553deeca10656a9a4cf1cbe3c931569e7d9
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-21 15:43:32 +0100

    added action to categories to return unpaginated list

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, parsers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Category, Recipe
 from .serializers import CategorySerializer, RecipeSerializer
 
@@ -6,6 +9,16 @@ from .serializers import CategorySerializer, RecipeSerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+
+    # Custom unpaginated endpoint
+
+    # 👈 Disable pagination
+    @action(detail=False, methods=['get'], pagination_class=None)
+    def all(self, request):
+        categories = self.get_queryset()
+        serializer = self.get_serializer(categories, many=True)
+        return Response(serializer.data)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
